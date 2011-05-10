1004e3de69a68f3557167daf368a40b7e4ef9892
Eric Holscher <eric@ericholscher.com>
2011-05-09 20:30:52 -0700

    Rewrote it in coffeescript.

--- readthedocs.js
+++ readthedocs.js
@@ -1,11 +1,30 @@
-var project_slug = "read-the-docs"
-var url = "http://readthedocs.org/api/v1/project/" + project_slug + "/?format=jsonp";
-$.ajax({
-  url: url,
-  dataType: 'jsonp',
-  success:function(data){
-      if (data.absolute_url){
-        alert("Project exists, with url: " + data['absolute_url']);
-      };
-  }
-});
+inject_html = (obj) ->
+  html = """
+           <li class="module-item">
+              <p class="module-item-title">
+                File:
+                <a href="#{obj['absolute_url']}"> - #{obj['project']['name']} - #{obj['name']}</a>
+              </p>
+              <p>
+                #{obj['text']}
+              </p>
+           </li>
+  """
+
+add_to_element = (id) ->
+  $("#" + id).submit (event)->
+    project_slug = $('#query')[0].value
+    url = "http://readthedocs.org/api/v1/file/search/?q=" + project_slug + "&format=jsonp";
+    $.ajax
+      url: url,
+      dataType: 'jsonp',
+      success: (data)->
+          if data?
+              text = ""
+              for obj in data.objects
+                  text += inject_html(obj)
+                  $("#result").html(text)
+    return false
+
+$(document).ready ->
+  add_to_element "rtd_query"
